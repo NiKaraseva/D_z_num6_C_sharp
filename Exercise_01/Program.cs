ace667cf6b2f3993a844dd145aed5f1ae7e94481
Nika <nikarasevas@gmail.com>
2022-10-04 00:16:57 +0300

    Посчитали количество чисел больше 0 в рандомном массиве

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4 */ 
+
+int[] array = GetArray(15, -50, 50);
+Console.WriteLine(String.Join(" ", array));
+
+Console.WriteLine($"Количество чисел в массиве больше 0 = {HowMuch(array)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+int HowMuch(int[] array )
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
